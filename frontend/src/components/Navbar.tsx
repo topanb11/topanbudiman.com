9c3489a60ebaa4c311965b105b8f2845e1291655
topanb11 <topanbudiman11@gmail.com>
2023-02-12 03:15:29 -0700

    Added animation

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
 	return (
-		<header>
-			<div className="bg-dark text-white h-18 flex flex-row items-center space-x-5 pl-4 py-4 ">
+			<motion.div 
+				initial={{x: -500}}
+				animate={{x: 0}}
+				transition={{x: {type: "spring" , duration: 1}}}
+				className="bg-dark text-white h-18 flex flex-row items-center space-x-5 pl-4 py-8"
+			>
 				<SocialIcon url="https://www.linkedin.com/in/topanb/" bgColor="#757083"/>
 				<SocialIcon url="https://github.com/topanb11" bgColor="#757083"/>
-			</div>
-		</header>
+			</motion.div>
 	)
 }
 export default Navbar;
