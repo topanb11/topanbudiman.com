620cc6e8217fe80d93b97bc223212a9ac21a7176
topanb11 <topanbudiman11@gmail.com>
2023-02-12 15:22:06 -0700

    Added experience

--- frontend/src/components/Experience.tsx
+++ frontend/src/components/Experience.tsx
@@ -1,7 +1,17 @@
+import { ExperienceData } from "../data";
+import ExperienceTile from "./ExperienceTile";
+
 const Experience = () => {
 	return (
-		<div id="experience-section" className="text-white"> 
-			<h1>Experience</h1>
+		<div id="experience-section" className="text-white h-screen"> 
+			<h1 className="text-3xl text-grey text-center mb-24 md:mb-36">E X P E R I E N C E</h1>
+			<ul>
+				{ExperienceData.map(value => {
+					return(
+						<ExperienceTile {...value}/>
+					)
+				})}
+			</ul>
 		</div>
 	)
 }
---
+++ frontend/src/components/ExperienceTile.tsx
@@ -0,0 +1,31 @@
+import { ExperienceCard } from "../interfaces/experience";
+import { motion } from "framer-motion";
+
+const ExperienceTile = ({order, company, title, location, duration, image }: ExperienceCard) => {
+
+	const EXPERIENCE_DETAILS = "italic text-grey md:text-lg"
+
+	return (
+		<motion.div
+			initial="hidden"
+			whileInView="visible"
+			viewport={{ once: true }}
+			transition={{ duration: 0.8}}
+			variants={{
+			visible: { x: 0, opacity: 1},
+			hidden: { x: order % 2 == 0 ? 100 : -100, opacity: 0.5 }
+			}}
+		>
+			<li key={order} className="flex flex-row justify-around items-center py-8 px-3">
+				<img src={image} className="w-24 h-24 md:w-48 md:h-48 rounded-sm"/>
+				<div className="text-white text-right w-80 md:w-96 border-r-4 border-grey pr-4 h-fit md:space-y-2">
+					<h3 className="font-extrabold text-xl md:text-3xl">{company}</h3>
+					<h3 className="text-xl">{title}</h3>
+					<h3 className={EXPERIENCE_DETAILS}>{duration}</h3>
+					<h3 className={EXPERIENCE_DETAILS}>{location}</h3>
+				</div>
+			</li>
+		</motion.div>
+	)
+}
+export default ExperienceTile;
