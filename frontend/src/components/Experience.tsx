9d211de6cece15a406dfbd949b52cd3aaa47efdb
topanb11 <topanbudiman11@gmail.com>
2023-02-23 00:06:32 -0700

    Fixed experience

--- frontend/src/components/Experience.tsx
+++ frontend/src/components/Experience.tsx
@@ -1,14 +1,30 @@
-import { ExperienceData } from "../data";
+import { useEffect, useState } from "react";
 import ExperienceTile from "./ExperienceTile";
+import { ExperienceCard } from "../interfaces/experience";
+import client from "../secrets";
 
 const Experience = () => {
+	const [experienceData, setExperienceData] = useState([]);
+
+	const getExperienceData = () => {
+		client.getEntries({content_type: "topan"})
+		.then((res) => {
+			let sortedData = res.items.sort((a: ExperienceCard, b: ExperienceCard) => (a.fields.order < b.fields.order ? -1 : 1));
+			setExperienceData(sortedData);
+		});
+	}
+
+	useEffect(() => {
+		getExperienceData();
+	}, [])
+
 	return (
 		<div id="experience-section" className="min-h-screen text-white overflow-hidden bg-dark pb-24"> 
-			<h1 className="text-3xl text-grey text-center mb-10">E X P E R I E N C E</h1>
+			<h1 className="text-3xl text-grey text-center mb-10 tracking-widest">E X P E R I E N C E</h1>
 			<ul>
-				{ExperienceData.map(value => {
+				{experienceData.map((value, index) => {
 					return(
-						<ExperienceTile {...value}/>
+						<ExperienceTile key={index} {...value.fields}/>
 					)
 				})}
 			</ul>
--- frontend/src/components/ExperienceTile.tsx
+++ frontend/src/components/ExperienceTile.tsx
@@ -1,7 +1,7 @@
 import { ExperienceCard } from "../interfaces/experience";
 import { motion } from "framer-motion";
 
-const ExperienceTile = ({order, company, title, location, duration, image }: ExperienceCard) => {
+const ExperienceTile = ({order, company, experience, location, date, headshot }: ExperienceCard) => {
 
 	const EXPERIENCE_DETAILS = "italic text-grey md:text-lg"
 
@@ -16,11 +16,11 @@ const ExperienceTile = ({order, company, title, location, duration, image }: Exp
 			}}
 		>
 			<li key={order} className="flex flex-row justify-around items-center py-8 px-3">
-				<img src={image} className="w-24 h-24 md:w-48 md:h-48 rounded-sm"/>
+				<img src={headshot.fields.file.url} className="w-24 h-24 md:w-48 md:h-48 rounded-sm"/>
 				<div className="text-white text-right w-80 md:w-96 border-r-2 border-grey pr-4 h-fit md:space-y-2">
 					<h3 className="font-extrabold text-xl md:text-3xl">{company}</h3>
-					<h3 className="text-xl">{title}</h3>
-					<h3 className={EXPERIENCE_DETAILS}>{duration}</h3>
+					<h3 className="text-xl">{experience}</h3>
+					<h3 className={EXPERIENCE_DETAILS}>{date}</h3>
 					<h3 className={EXPERIENCE_DETAILS}>{location}</h3>
 				</div>
 			</li>
