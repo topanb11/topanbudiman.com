3131ca8b6f1f3b25736739c917a88028e821d054
topanb11 <topanbudiman11@gmail.com>
2023-03-31 23:13:14 -0600

    Fixed experience

--- frontend/src/components/Experience.tsx
+++ frontend/src/components/Experience.tsx
@@ -1,34 +1,36 @@
-import { useEffect, useState } from "react";
-import ExperienceTile from "./ExperienceTile";
-import { ExperienceCard } from "../interfaces/experience";
-import client from "../secrets";
+import { useEffect, useState } from 'react';
+import ExperienceTile from './ExperienceTile';
+import { ExperienceCard } from '../interfaces/experience';
+import client from '../secrets';
 
 const Experience = () => {
-	const [experienceData, setExperienceData] = useState([]);
+	const [experienceData, setExperienceData] = useState<ExperienceCard[]>([]);
 
 	const getExperienceData = () => {
-		client.getEntries({content_type: "topan"})
-		.then((res) => {
-			let sortedData = res.items.sort((a: ExperienceCard, b: ExperienceCard) => (a.fields.order < b.fields.order ? -1 : 1));
-			setExperienceData(sortedData);
+		client.getEntries<ExperienceCard>({ content_type: 'topan' }).then((res) => {
+			const entries = res.items.map((entry) => entry.fields);
+			setExperienceData(entries);
 		});
-	}
+	};
 
 	useEffect(() => {
 		getExperienceData();
-	}, [])
+	}, []);
 
 	return (
-		<div id="experience-section" className="min-h-screen text-white overflow-hidden bg-dark pb-24"> 
-			<h1 className="text-3xl text-grey text-center mb-10 tracking-widest">E X P E R I E N C E</h1>
+		<div
+			id="experience-section"
+			className="min-h-screen text-white overflow-hidden bg-dark pb-24"
+		>
+			<h1 className="text-3xl text-grey text-center mb-10 tracking-widest">
+				E X P E R I E N C E
+			</h1>
 			<ul>
-				{experienceData.map((value, index) => {
-					return(
-						<ExperienceTile key={index} {...value.fields}/>
-					)
+				{experienceData.map((data, index) => {
+					return <ExperienceTile key={index} {...data} />;
 				})}
 			</ul>
 		</div>
-	)
-}
+	);
+};
 export default Experience;
